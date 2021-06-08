d94bf628fe6f5c7a743014d0b7160375481107c2
whillz <joluwapelumi.williams@gmail.com>
2021-06-08 16:39:20 +0200

    Replaced "four_in_a_row_duels_ai.h"
    Replaced "four_in_a_row_duels_ai.h" due to some changes in the .yaml file and the structure of the mechanics file.

--- four_in_a_row_duels/include/duels/four_in_a_row_duels/four_in_a_row_duels_ai.h
+++ four_in_a_row_duels/include/duels/four_in_a_row_duels/four_in_a_row_duels_ai.h
@@ -19,13 +19,13 @@ public:
 private:
   int difficulty = 1;
 
-  const int SEED = 5;
+  //const int SEED = 5;
   int ROWS = 6; // edit: we have to find a way to pass the row values from the grid generated in the mechanics file
   int COLUMNS = 8; // edit: we have to find a way to pass the column values from the grid generated in the mechanics file
 
   std::vector<int> column_pool; // vector containing the columns that we can play in
 
-  bool EMPTY_TOKEN = false;
+  const bool EMPTY_TOKEN = false;
   bool ai_initialized = false;
   std::vector<std::vector<bool>> my_grid;
   std::vector<std::vector<bool>> opponent_grid;
@@ -33,7 +33,7 @@ private:
 
   void initializeAI();
   void initializeGrids();
-  void reconstructGrids();
+  void updateGrids();
 
 };
 }
